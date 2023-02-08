b2124da315c925df50d23145e6f17ba81cc0b3e9
BionXP <112658830+BionXP@users.noreply.github.com>
2023-02-08 22:50:28 +0700

    Update telegraph.py

--- PyroZen/modules/basic/telegraph.py
+++ PyroZen/modules/basic/telegraph.py
@@ -19,7 +19,7 @@ from PyroZen.helpers.tools import *
 from .help import *
 
 telegraph = Telegraph()
-r = telegraph.create_account(short_name="PyroMan-Userbot")
+r = telegraph.create_account(short_name="telegram")
 auth_url = r["auth_url"]
 
 
